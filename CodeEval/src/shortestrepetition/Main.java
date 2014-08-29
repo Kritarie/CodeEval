6bef4d4729e083faa82d1dc20fedf29a839ea6fa
Kritarie <seanamos@seanamos.net>
2014-08-29 18:03:20 -0400

    Completed Shortest Repetition Challenge - Easy

---
+++ CodeEval/src/shortestrepetition/Main.java
@@ -0,0 +1,40 @@
+package shortestrepetition;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+	    File file = new File(args[0]);
+	    BufferedReader in = new BufferedReader(new FileReader(file));
+	    String line;
+	    while ((line = in.readLine()) != null) {
+	    	for (int i = 1; i <= line.length(); i++) {
+	    		if (isRepeated(line.substring(0, i), line)) {
+	    			System.out.println(i);
+	    			break;
+	    		}
+	    	}
+	    }
+	    
+	    in.close();
+	    System.out.print("\n");
+	}
+	
+	private static boolean isRepeated(String sub, String og) {
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			sb.append(sub);
+			if (og.equalsIgnoreCase(sb.toString())) {
+				return true;
+			}
+			
+			if (sb.toString().length() > og.length()) {
+				return false;
+			}
+		}
+	}
+}
