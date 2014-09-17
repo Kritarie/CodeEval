f7518205df8316368460768049abd824cbbeb4d3
Kritarie <seanamos@seanamos.net>
2014-09-17 18:39:24 -0400

    Adjusted for multiline pattern matching

--- CodeEval/src/sequencetransformation/Main.java
+++ CodeEval/src/sequencetransformation/Main.java
@@ -4,13 +4,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Main {
 
 //https://www.codeeval.com/open_challenges/130/
 	
 	public static void main(String[] args) throws IOException {
-		File file = new File(args[0]);
+		File file = new File("txts/sequencetransformation.txt");
 		BufferedReader in = new BufferedReader(new FileReader(file));
 		String line;
 		String[] params;
@@ -22,7 +23,8 @@ public class Main {
 				sb.append(Character.getNumericValue(params[0].charAt(i)) == 1 ? "([A]+|[B]+)" : "[A]+");
 			}
 			
-			if (params[1].matches(sb.toString())) {
+			Pattern p = Pattern.compile(sb.toString(), Pattern.MULTILINE);
+			if (p.matcher(params[1]).find()) {
 				System.out.println("Yes");
 			} else {
 				System.out.println("No");
