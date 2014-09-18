147015f3a054ccc6626bfaafd9e8ee1342bf16ca
Kritarie <seanamos@seanamos.net>
2014-09-17 20:20:52 -0400

    Lost in Translation Challenge Partially Complete - Moderate

---
+++ CodeEval/src/lostintranslation/Main.java
@@ -0,0 +1,59 @@
+package lostintranslation;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+//https://www.codeeval.com/open_challenges/121/
+
+	public static void main(String[] args) throws IOException {
+		File file = new File(args[0]);
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		String line;
+		char[] chars;
+		Map<Character, Character> map = new HashMap<Character, Character>();
+		map.put('a', 'y');
+		map.put('b', 'h');
+		map.put('c', 'e');
+		map.put('d', 's');
+		map.put('e', 'o');
+		map.put('f', 'c');
+		map.put('g', 'v');
+		map.put('h', 'b');
+		map.put('i', 'd');
+		map.put('j', 'u');
+		map.put('k', 'i');
+		map.put('l', 'g');
+		map.put('m', 'l');
+		map.put('n', 'b');
+		map.put('o', 'k');
+		map.put('p', 'r');
+		map.put('q', 'z');
+		map.put('r', 't');
+		map.put('s', 'n');
+		map.put('t', 'w');
+		map.put('u', 'j');
+		map.put('v', 'p');
+		map.put('w', 'f');
+		map.put('x', 'm');
+		map.put('y', 'a');
+		map.put('z', 'q');
+		
+		while ((line = in.readLine()) != null) {
+			chars = line.toCharArray();
+			for (int i = 0; i < chars.length; i++) {
+				if (chars[i] != ' ') {
+					chars[i] = map.get(chars[i]);
+				}
+			}
+			System.out.println(new String(chars));
+		}
+		
+		in.close();
+	}
+}
